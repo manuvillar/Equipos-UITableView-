d394b93538894a9b1f9fe1900fe72de92451df70
Manuel J. Villar <mjvillar@iesoretania.es>
2023-03-01 20:44:33 +0100

    Commit inicial

--- Equipos (UITableView)/ViewController.swift
+++ Equipos (UITableView)/ViewController.swift
@@ -7,13 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var equipos: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        equipos = ["Atlético de Madrid", "Barcelona", "Deportivo de la Coruña", "Las Palmas", "Málaga", "Rayo Vallecano", "Sporting", "Real Sociedad", "Espanyol", "Mallorca", "Valladolid", "Éibar", "Ponferradina", "Albacete"]
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return equipos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda:UITableViewCell=UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "micelda")
+          celda.textLabel?.text  = equipos[indexPath.row]
+          celda.imageView!.image = UIImage(named: equipos[indexPath.row])!
+          return celda
+    }
 }
 
